90398ab318f9f3cfc7cd63cce578048dfd701970
n1en1k <niko.nieminen93@gmail.com>
2023-04-02 06:12:57 +0300

    added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Movies from './components/Movies'
+import Navbar from "./components/Navbar/Navbar";
+import Movies from "./components/Movies";
+import Actors from "./components/Actors";
+import Directors from "./components/Directors";
 
 function App() {
-  return <div className="App">
-    <Movies />
-  </div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Movies />} />
+          <Route path="/actors" element={<Actors />} />
+          <Route path="/directors" element={<Directors />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
