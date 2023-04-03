77defe0994077fdaa4a563f55c1c9c552c97d83f
n1en1k <niko.nieminen93@gmail.com>
2023-04-03 14:20:26 +0300

    tested sql queries on server/routes/index

--- server/routes/index.routes.js
+++ server/routes/index.routes.js
@@ -20,4 +20,28 @@ router.get("/ping", async (req, res) => {
   }
 });
 
+// returns movie data with director name
+router.get("/ping2", async (req, res) => {
+  try {
+    const [result] = await pool.query(
+      "SELECT movie.id, movie.name AS movieName, movie.director_id, director.name AS directorName, movie.year FROM movie JOIN director ON director.id = movie.director_id"
+    );
+    res.json(result);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+});
+
+// returns movie data with director name and actors (multiple same movie records because of multiple actors in the same movie)
+router.get("/ping3", async (req, res) => {
+  try {
+    const [result] = await pool.query(
+      "SELECT movie.id AS movieID, movie.name AS movieName, movie.director_id, director.name AS directorName, movie.year, starring.actor_id, actor.name AS actorName FROM movie JOIN director ON director.id = movie.director_id JOIN starring ON movie.id = starring.movie_id JOIN actor ON actor.id = starring.actor_id"
+    );
+    res.json(result);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+});
+
 export default router;
